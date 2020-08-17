1607300068e620d956407776b5c375403ccbdc7b
Dilain Gunasekara <adsgunasekara18@gmail.com>
2020-08-17 07:11:16 +0530

    added register

---
+++ app/src/main/java/com/example/juicebar/Drink.java
@@ -0,0 +1,14 @@
+package com.example.juicebar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Drink extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+    }
+}
